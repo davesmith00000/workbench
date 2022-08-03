084423c5287a3200a8811468ef9ccb56e2b54a73
Dave Smith <davesmith.00000@gmail.com>
2022-08-03 12:22:03 +0100

    Updated Scala.js + SJS deps, compile failing

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,4 +1,5 @@
 import sbt._
+import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 
 object Dependencies {
@@ -8,8 +9,8 @@ object Dependencies {
   val akka = "com.typesafe.akka" %% "akka-actor" % "2.4.19"
 
   // js and shared dependencies
-  val autowire = Def.setting("com.lihaoyi" %%% "autowire" % "0.2.6")
-  val dom = Def.setting("org.scala-js" %%% "scalajs-dom" % "0.9.3")
-  val upickle = Def.setting("com.lihaoyi" %%% "upickle" % "0.4.4")
+  val autowire = Def.setting("com.lihaoyi" %%% "autowire" % "0.3.3")
+  val dom = Def.setting("org.scala-js" %%% "scalajs-dom" % "2.2.0")
+  val upickle = Def.setting("com.lihaoyi" %%% "upickle" % "2.0.0")
 
 }
